07f91871597dcd336719a3b57eb348b069d0e9cd
Bruno Resende <rresendebruno@gmail.com>
2022-09-22 23:06:19 -0300

    consumo de API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-// 2caa8ff1e9e92d80dc7730c872ead0e9
+// Variaveis e seleção de elementos
+const apiKey = "2caa8ff1e9e92d80dc7730c872ead0e9";
+const countryApiURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+// Funções
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  return data;
+};
+
+const showWeather = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", countryApiURL + data.sys.country);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+};
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
+  showWeather(city);
+});
